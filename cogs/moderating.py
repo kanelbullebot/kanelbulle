a76ac37a08fcb8be0a407f929da7ec636d89240f
trilleplay <trilleplay@users.noreply.github.com>
2018-09-17 22:10:50 +0200

    Add neat ban command.

--- cogs/moderating.py
+++ cogs/moderating.py
@@ -10,12 +10,17 @@ class ModCog:
     @commands.command(name='modtest')
     @commands.has_role('Moderator')
     async def only_me(self, ctx):
-        
+
         await ctx.send(f'Hello Moderator This command can only be used by you!!')
-        
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name='ban')
+    async def banusr(self, ctx, member: discord.Member, reason: str):
+        await ctx.message.guild.ban(member)
+        await ctx.send("User has been banned. oof.")
 
 
-        
+
 
 # Add moderating cog to main instance.
 def setup(bot):
