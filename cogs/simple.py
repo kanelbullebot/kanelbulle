5680c787836b14ef913e789d86db6bb939d20817
trilleplay <trilleplay@users.noreply.github.com>
2018-09-23 22:26:02 +0200

    Update simple.py

--- cogs/simple.py
+++ cogs/simple.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import ast
+import contextlib
+import traceback
+import textwrap
+import io
 
 class SimpleCog:
     """SimpleCog"""
@@ -45,6 +50,78 @@ class SimpleCog:
 
 
         await ctx.send(content='**Kanelbulle**', embed=embed)
+ # Eval Command - https://github.com/Rapptz/RoboDanny by Rapptz read credit.md for License
+    def insert_returns(body):
+        # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        # for if statements, we insert returns into the body and the orelse
+        if isinstance(body[-1], ast.If):
+            insert_returns(body[-1].body)
+            insert_returns(body[-1].orelse)
+
+        # for with blocks, again we insert returns into the body
+        if isinstance(body[-1], ast.With):
+            insert_returns(body[-1].body)
+
+
+    @commands.command(hidden=True, name='eval')
+    @commands.is_owner()
+    async def eval(self, ctx:commands.Context, *, code: str):
+        output = None
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message
+        }
+
+        env.update(globals())
+
+        if code.startswith('```'):
+            code = "\n".join(code.split("\n")[1:-1])
+
+        out = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(code, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            output = f'{e.__class__.__name__}: {e}'
+        else:
+            func = env['func']
+            try:
+                with contextlib.redirect_stdout(out):
+                    ret = await func()
+            except Exception as e:
+                value = out.getvalue()
+                output = f'{value}{traceback.format_exc()}'
+            else:
+                value = out.getvalue()
+                if ret is None:
+                    if value:
+                        output = value
+                else:
+                    output = f'{value}{ret}'
+        if output is not None:
+            await ctx.send(output)
+        else:
+            await ctx.send("I did a thing!")
+
+    async def init_eval(self, ctx, pages):
+        page = pages[0]
+        num = len(pages)
+        return f"**Eval output 1/{num}**\n```py\n{page}```", None, num > 1, []
+
+    async def update_eval(self, ctx, message, page_num, action, data):
+        pages = data["pages"]
+        page, page_num = Pages.basic_pages(pages, page_num, action)
+        return f"**Eval output {page_num + 1}/{len(pages)}**\n```py\n{page}```", None, page_num
 
     async def on_member_ban(self, guild, user):
         """Event Listener which is called when a user is banned from the guild.
