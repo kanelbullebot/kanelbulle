261036bb583dd492352277537311f04659dbb89f
trilleplay <trille@trilleplay.net>
2018-12-01 23:34:52 +0100

    Update simple.py

--- cogs/simple.py
+++ cogs/simple.py
@@ -122,7 +122,6 @@ class SimpleCog:
             await ctx.send(f"I found nothing about `{tosearch}` on Wikipedia, did you commit a typo?")
 
 
-# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
-# When we load the cog, we use the name of the file.
+
 def setup(bot):
     bot.add_cog(SimpleCog(bot))
