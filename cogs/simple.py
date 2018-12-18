ea66babf62efdd6f1b0109b19a868ef2fa9a12e1
trilleplay <trille@trilleplay.net>
2018-12-18 20:15:56 +0100

    Update simple.py

--- cogs/simple.py
+++ cogs/simple.py
@@ -15,8 +15,8 @@ class SimpleCog:
 
     @commands.command()
     @commands.guild_only()
-    async def serverstats(self, ctx, guildid: discord.Guild=None):
-        if guildid is None:
+    async def serverstats(self, ctx, *, guildid: int:0):
+        if guildid is 0:
             serverstats = discord.Embed()
             serverstats.set_thumbnail(url=ctx.guild.icon_url)
             serverstats.add_field(name="Guild Name", value=ctx.guild.name, inline=False)
