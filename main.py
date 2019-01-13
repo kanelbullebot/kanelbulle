670667b95231ca66f5597205e8d40a3515e683aa
trilleplay <trille@trilleplay.net>
2019-01-13 13:18:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_prefix(bot, message):
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-initial_extensions = ['cogs.simple',
+initial_extensions = ['cogs.core',
                       'cogs.admin',
                       'cogs.moderating',
                       'cogs.fortnite']
