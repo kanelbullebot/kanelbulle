9f580c91f83c183c6185d74b613dd26aa193fe0a
trilleplay <trille@trilleplay.net>
2019-01-22 16:58:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -78,28 +78,6 @@ async def on_guild_join(guild):
             except discord.Forbidden:
                 pass
 
-
-class DiscordBotsOrgAPI:
-    """Handles interactions with the discordbots.org API"""
-
-    def __init__(self, bot):
-        self.bot = bot
-        self.token = returnconfig['dbltoken']
-        self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
-
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-
-        while True:
-            logger.info('Sending server count of to DBL Weeeeee!')
-            try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
-
 @bot.event
 async def on_message(ctx):
     if ctx.author.bot:
