4eb28b06d5d116b83ec5a51c0447d81db6d5ec58
trilleplay <trille@trilleplay.net>
2019-01-25 19:10:40 +0100

    Update fortnite.py

--- bot/cogs/fortnite.py
+++ bot/cogs/fortnite.py
@@ -12,89 +12,37 @@ class FortniteCog:
         self.bot = bot
 
 
-    @commands.command(name='fstatssquad')
-    async def fortnitestatssquad(self, ctx, *, username: str):
-        """Gets a players squad Fortnite stats."""
-        url = "https://api.fortnitetracker.com/v1/profile/pc/" + "/" + (username)
+    @commands.command(name='fstats')
+    async def fstats(self, ctx, *, username: str):
+        url = "https://api.scoutsdk.com/graph"
+        headers = {
+            'Accept': "application/com.scoutsdk.graph+json; version=1.1.0; charset=utf8",
+            'Content-Type': "application/json",
+            'Scout-App': "Scout token here",
+            'cache-control': "no-cache"
+            }
 
-        headerssquad = {
-            'TRN-Api-Key': "FortniteTrackerAPIKEY",
-            'Cache-Control': "no-cache",
-        }
+        idlookup = "{\"query\":\"{\\n  players(title: \\\"fortnite\\\", platform: \\\"epic\\\", identifier: \\\"" + (username) + "\\\", exact: true) {\\n    results {\\n      player {\\n        playerId\\n        handle\\n      }\\n    }\\n  }\\n}\"}"
+        responseid = requests.request("POST", url, data=idlookup, headers=headers)
+        responseidparsed = responseid.json()
+        userid = responseidparsed["data"]["players"]["results"][0]["player"]["playerId"]
+        data = "{\"query\":\"{\\n  player(title: \\\"fortnite\\\", id: \\\"" + (userid) + "\\\", segment: \\\"*\\\") {\\n    id\\n    metadata {\\n      key\\n      name\\n      value\\n      displayValue\\n    }\\n    stats {\\n      metadata {\\n        key\\n        name\\n        isReversed\\n      }\\n      value\\n      displayValue\\n    }\\n  }\\n}\"}"
 
-        responsesquad = requests.get(url, headers=headerssquad)
-        print(responsesquad.text)
-        jsfstatssquad = responsesquad.json()
+        responsestats = requests.post(url, headers=headers, data=data)
+        responsestatsparsed = responsestats.json()
 
-        fortnitestatsembedsquad = discord.Embed(title='Squad Fortnite Stats',
-                              description='Showing Squad Fortnite stats! Through the magic of Kanelbulle.',
+        fstatsembed = discord.Embed(title='Fortnite Stats',
+                              description='Showing Fortnite stats! Through the magic of Kanelbulle.',
                               colour=0x98FB98)
 
-        fortnitestatsembedsquad.add_field(name="Username", value=(username))
-        fortnitestatsembedsquad.add_field(name="Lifetime K/D", value=(jsfstatssquad["stats"]["p9"]["kd"]["value"]))
-        fortnitestatsembedsquad.add_field(name="Lifetime Kills", value=(jsfstatssquad["stats"]["p9"]["kills"]["value"]))
-        fortnitestatsembedsquad.add_field(name="Matches Played", value=(jsfstatssquad["stats"]["p9"]["matches"]["value"]))
-        fortnitestatsembedsquad.add_field(name="Wins", value=(jsfstatssquad["stats"]["p9"]["top1"]["value"]))
+        fstatsembed.add_field(name="Username", value=(username))
+        fstatsembed.add_field(name="Lifetime K/D", value=(responsestatsparsed["data"]["player"]["stats"][8]["displayValue"]))
+        fstatsembed.add_field(name="Lifetime Kills", value=(responsestatsparsed["data"]["player"]["stats"][0]["displayValue"]))
+        fstatsembed.add_field(name="Matches Played", value=(responsestatsparsed["data"]["player"]["stats"][2]["displayValue"]))
+        fstatsembed.add_field(name="Lifetime Wins", value=(responsestatsparsed["data"]["player"]["stats"][3]["displayValue"]))
 
-        fstatssquad = "Showing Fortnite Squad Stats for: " + (username)
-        await ctx.send(content=fstatssquad, embed=fortnitestatsembedsquad)
-
-    @commands.command(name='fstatssolo')
-    async def fortnitestatssolo(self, ctx, *, username: str):
-        """Gets a players solo Fortnite stats."""
-        url = "https://api.fortnitetracker.com/v1/profile/pc/" + "/" + (username)
-
-        headerssolo = {
-            'TRN-Api-Key': "FortniteTrackerAPIKEY",
-            'Cache-Control': "no-cache",
-        }
-
-        responsesolo = requests.get(url, headers=headerssolo)
-        print(responsesolo.text)
-        jsfstatssolo = responsesolo.json()
-
-        fortnitestatsembedsolo = discord.Embed(title='Solo Fortnite Stats',
-                              description='Showing Solo Fortnite stats! Through the magic of Kanelbulle.',
-                              colour=0x98FB98)
-
-        fortnitestatsembedsolo.add_field(name="Username", value=(username))
-        fortnitestatsembedsolo.add_field(name="Lifetime K/D", value=(jsfstatssolo["stats"]["p2"]["kd"]["value"]))
-        fortnitestatsembedsolo.add_field(name="Lifetime Kills", value=(jsfstatssolo["stats"]["p2"]["kills"]["value"]))
-        fortnitestatsembedsolo.add_field(name="Matches Played", value=(jsfstatssolo["stats"]["p2"]["matches"]["value"]))
-        fortnitestatsembedsolo.add_field(name="Wins", value=(jsfstatssolo["stats"]["p2"]["top1"]["value"]))
-
-        fstatssolo = "Showing Fortnite Solo Stats for: " + (username)
-        await ctx.send(content=fstatssolo, embed=fortnitestatsembedsolo)
-
-    @commands.command(name='fstatsduo')
-    async def fortnitestatsduo(self, ctx, *, username: str):
-        """Gets a players duo Fortnite stats."""
-        url = "https://api.fortnitetracker.com/v1/profile/pc/" + "/" + (username)
-
-        headersduo = {
-            'TRN-Api-Key': "FortniteTrackerAPIKEY",
-            'Cache-Control': "no-cache",
-        }
-
-        responseduo = requests.get(url, headers=headersduo)
-        print(responseduo.text)
-        jsfstatsduo = responseduo.json()
-
-        fortnitestatsembedduo = discord.Embed(title='Duo Fortnite Stats',
-                              description='Showing Duo Fortnite stats! Through the magic of Kanelbulle.',
-                              colour=0x98FB98)
-
-        fortnitestatsembedduo.add_field(name="Username", value=(username))
-        fortnitestatsembedduo.add_field(name="Lifetime K/D", value=(jsfstatsduo["stats"]["p10"]["kd"]["value"]))
-        fortnitestatsembedduo.add_field(name="Lifetime Kills", value=(jsfstatsduo["stats"]["p10"]["kills"]["value"]))
-        fortnitestatsembedduo.add_field(name="Matches Played", value=(jsfstatsduo["stats"]["p10"]["matches"]["value"]))
-        fortnitestatsembedduo.add_field(name="Matches Played", value=(jsfstatsduo["stats"]["p10"]["top1"]["value"]))
-
-        fstatsduo = "Showing Fortnite Duo Stats for: " + (username)
-        await ctx.send(content=fstatsduo, embed=fortnitestatsembedduo)
-
-
-        # await ctx.send(f"{len(fstats.decode("json"))}")
+        message = "Showing Stats for: " + (username)
+        await ctx.send(content=message, embed=fstatsembed)
 
 
 # Add Fortnite cog to main instance.
