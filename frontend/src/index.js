c5f7533f61b0047121f0eafaf9d1fd05ed7d1bc5
trilleplay <trille@trilleplay.net>
2019-01-27 15:04:31 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.jsx";
-import RTLLayout from "layouts/RTL/RTL.jsx";
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
